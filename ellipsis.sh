9d43f5d6284508022a9df8ec158f74f30fea3345
G. Roggemans <g.roggemans@grog.be>
2016-03-08 20:10:34 +0100

    Added install and uninstall hooks
    Install hook adds a warning in the crontab. Uninstall removes this
    warning.

--- ellipsis.sh
+++ ellipsis.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 ##############################################################################
 
+cron_warning="# Ellipsis-Cron managed file. Edit with care!"
+
+##############################################################################
+pkg.install() {
+
+    # Add crontab warning if not present
+    current_crontab="$(crontab -l 2> /dev/null)"
+    if ! grep "$cron_warning" <<< "$current_crontab" > /dev/null; then
+        crontab <<< \
+"$cron_warning
+$current_crontab"
+    fi
+}
+
+##############################################################################
+
 pkg.link() {
     fs.link_file "$PKG_PATH/bin/ellipsis-cron" "$ELLIPSIS_PATH/bin/ellipsis-cron"
 }
@@ -9,6 +25,18 @@ pkg.link() {
 
 pkg.unlink() {
     rm "$ELLIPSIS_PATH/bin/ellipsis-cron"
+}
+
+##############################################################################
+
+pkg.uninstall(){
+    "$PKG_PATH/bin/ellipsis-cron" remove all
+
+    # Remove crontab warning if present
+    local current_crontab="$(crontab -l 2> /dev/null)"
+    if grep "$cron_warning" <<< "$current_crontab" > /dev/null; then
+        crontab -l | grep -v "$cron_warning" | crontab
+    fi
 }
 
 ##############################################################################
