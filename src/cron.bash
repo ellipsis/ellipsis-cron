d5f01bf980484eec48c44199f45371daf0eef845
G. Roggemans <g.roggemans@grog.be>
2016-03-09 14:12:45 +0100

    Rough first implementation of cron functions

--- src/cron.bash
+++ src/cron.bash
@@ -10,25 +10,100 @@ load log
 ##############################################################################
 
 cron.add() {
-    :
+    local name="$1"
+    local time="$2"
+    local cmd="$3"
+
+    # Remove, then add
+    cron.remove "$name"
+
+    # Buffer crontab content
+    local crontab="$(crontab -l 2> /dev/null)"
+    local crontab="\
+$crontab
+# Ellipsis-Cron : $name
+$time $cmd"
+    echo "$crontab" | crontab
 }
 
 ##############################################################################
 
 cron.remove() {
-    :
+    local name="$1"
+
+    # Buffer crontab content
+    local crontab="$(crontab -l 2> /dev/null)"
+
+    if [ "$name" = "all" ]; then
+        # TODO : Remove all jobs added by ellipsis-cron
+        msg.bold "TODO"
+    else
+        # Build sed string
+        local sed_string="/^# Ellipsis-Cron : $name\$/ { N; d; }"
+        sed "$sed_string" <<< "$crontab" | crontab
+    fi
 }
 
 ##############################################################################
 
 cron.enable() {
-    :
+    local name="$1"
+
+    # Buffer crontab content
+    local crontab="$(crontab -l 2> /dev/null)"
+
+    # Build search string to get job
+    local awk_string="f{print; f=0} /^# Ellipsis-Cron : $name\$/{f=1}"
+
+    # Get the job
+    local job="$(awk "$awk_string" <<< "$crontab")"
+
+    # Remove leading #'s
+    if [ "${job:0:1}" = "#" ]; then
+        job="${job:1}"
+    fi
+
+    # Extract time and command
+    if [ "${job:0:1}" = "@" ]; then
+        local time="$(cut -d ' ' -f1 <<< "$job")"
+        local cmd="$(cut -d ' ' --complement -f1 <<< "$job")"
+    else
+        local time="$(cut -d ' ' -f1-5 <<< "$job")"
+        local cmd="$(cut -d ' ' --complement -f1-5 <<< "$job")"
+    fi
+
+    cron.add "$name" "$time" "$cmd"
 }
 
 ##############################################################################
 
 cron.disable() {
-    :
+    local name="$1"
+
+    # Buffer crontab content
+    local crontab="$(crontab -l 2> /dev/null)"
+
+    # Build search string to get job
+    local awk_string="f{print; f=0} /^# Ellipsis-Cron : $name\$/{f=1}"
+
+    # Get the job
+    local job="$(awk "$awk_string" <<< "$crontab")"
+
+    # Remove leading #'s (if disabled already)
+    if [ "${job:0:1}" = "#" ]; then
+        job="${job:1}"
+    fi
+
+    # Extract time and command
+    if [ "${job:0:1}" = "@" ]; then
+        local time="$(cut -d ' ' -f1 <<< "$job")"
+        local cmd="$(cut -d ' ' --complement -f1 <<< "$job")"
+    else
+        local time="$(cut -d ' ' -f1-5 <<< "$job")"
+        local cmd="$(cut -d ' ' --complement -f1-5 <<< "$job")"
+    fi
+
+    cron.add "$name" "#$time" "$cmd"
 }
 
 ##############################################################################
