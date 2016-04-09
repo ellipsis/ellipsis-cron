e83f08c17109a94aa06875a109f7e4a4be6d43d1
G. Roggemans <g.roggemans@grog.be>
2016-04-09 19:17:37 +0200

    Updated ellipsis.sh file

--- ellipsis.sh
+++ ellipsis.sh
@@ -5,13 +5,16 @@ cron_warning="# Ellipsis-Cron managed file. Edit with care!"
 
 ##############################################################################
 pkg.install() {
+    local CRONTAB="$(crontab -l 2> /dev/null)"
 
     # Add crontab warning if not present
-    current_crontab="$(crontab -l 2> /dev/null)"
-    if ! grep "$cron_warning" <<< "$current_crontab" > /dev/null; then
-        crontab <<< \
-"$cron_warning
-$current_crontab"
+    if ! grep "$cron_warning" <<< "$CRONTAB" > /dev/null; then
+        # Only add newline if crontab is not empty
+        if [ -n "$CRONTAB" ]; then
+            crontab <<< "$cron_warning"$'\n'"$CRONTAB"
+        else
+            crontab <<< "$cron_warning"
+        fi
     fi
 }
 
@@ -30,10 +33,20 @@ pkg.unlink() {
 ##############################################################################
 
 pkg.uninstall(){
+    local CRONTAB="$(crontab -l 2> /dev/null)"
+
     # Remove crontab warning if present
-    local current_crontab="$(crontab -l 2> /dev/null)"
-    if grep "$cron_warning" <<< "$current_crontab" > /dev/null; then
-        crontab -l | grep -v "$cron_warning" | crontab
+    if grep "$cron_warning" <<< "$CRONTAB" > /dev/null; then
+        cron_warning="$(sed 's/[\/&]/\\&/g' <<< "$cron_warning")"
+
+        # Remove warning
+        local sed_string="/^$cron_warning\$/d"
+        CRONTAB="$(sed "$sed_string" <<< "$CRONTAB")"
+
+        if ! crontab <<< "$CRONTAB"; then
+            log.error "Could not restore crontab file"
+            return 1
+        fi
     fi
 }
 
